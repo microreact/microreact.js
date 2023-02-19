1df6608e1dbd5425ea077fc6c0833e27d033d552
Khalil <khalil.abudahab@cgps.group>
2023-02-19 09:33:00 +0000

    Create store.js

---
+++ api-client/files/store.js
@@ -0,0 +1,10 @@
+import apiRequest from '../api-request';
+
+export function storeFile(apiUrl, fileInput) {
+  return apiRequest({
+    method: "post",
+    baseURL: apiUrl,
+    url: "/files/store/",
+    data: fileInput,
+  });
+}
